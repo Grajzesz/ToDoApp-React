31110e06ab2c2e1c2e326e1d2e23ecae7a67600a
Mariusz Buskiewicz <grajzesz@gmail.com>
2019-05-09 12:21:16 +0200

    oki

---
+++ src/TodoItems.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+class TodoItems extends Component {
+
+  render() {
+
+    return <ul></ul>
+  }
+}
+export default TodoItems
---
+++ src/TodoList.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+class TodoList extends Component {
+    render() {
+        return <div />;
+    }
+}
+export default TodoList;
