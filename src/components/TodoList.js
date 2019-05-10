3e300001ff96be36a7d2fbea1ed54471b6ff92a5
Mariusz Buskiewicz <grajzesz@gmail.com>
2019-05-10 14:18:15 +0200

    oki

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,18 +2,12 @@ import React from 'react';
 import TodoItems from './TodoItems';
 
 const TodoList = props => {
-   
+
     const tasks = props.tasks.map(task => (
         <TodoItems key={task.id} task={task} />
     ));
 
-    return (
-        <div className="list">
-
-            {tasks}
-
-        </div>
-    );
+    return <div className="list">{tasks}</div>;
 };
 
 export default TodoList;
